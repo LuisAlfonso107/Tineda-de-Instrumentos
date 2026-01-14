d12273b61ae04dca633bf7ffd348d0067779f773
LuisAlfonso107 <luisalfonsogarcialagos9@gmail.com>
2026-01-14 11:59:26 +0100

    actualizacion de admin y client

--- js/dashboardCliente.js
+++ js/dashboardCliente.js
@@ -1,39 +1,57 @@
 import { dashboardClienteTemplate } from '../components/dashboardCliente.template.js';
 
 export const dashboardCliente = {
-    container: document.getElementById("dashboardCliente-main"),
-    
-    init() {
-        
-const user = JSON.parse(localStorage.getItem("user")) || [];
-const sessionStatus = JSON.parse(localStorage.getItem("sessionStatus")) || true;
-const usuarioActivo = user.isActive
+  container: document.getElementById("dashboardCliente-main"),
 
+  init() {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const usuarioActivo = users.find(user => user.isActive === true);
 
-if (usuarioActivo && user.role === "admin" && sessionStatus === true) {
-    const saludo = document.getElementById("user");
-    saludo.innerText = `Bienvenido ${user.name} 🎵`;
-    window.location.href = "./pages/dashBoardAdmin.html";
-
-} else if (usuarioActivo && user.role === "client" && sessionStatus === true) {
-    window.location.href = "./pages/dashboardCliente.html";
-   
-  
-} else {
- 
-    window.location.href = "../../index.html";
-}
-
-        
-        const botonCerrarSesion = document.getElementById("logout");
-        if (botonCerrarSesion) {
-            botonCerrarSesion.addEventListener("click", () => {
-                usuarioActivo.isActive = false;
-                localStorage.setItem("users", JSON.stringify(users));
-                window.location.href = "/index.html";
-            });
-        }
+    // lo mando a admin 
+    if (usuarioActivo?.isActive === true && usuarioActivo.role === "admin") {
+      window.location.href = "../pages/dashBoardAdmin.html";
+      return;
     }
+
+    // lo mando a cliente
+    if (usuarioActivo?.isActive === true && usuarioActivo.role === "client") {
+
+      const saludo = document.getElementById("user");
+      if (saludo) {
+        saludo.innerText = `Hola, ${usuarioActivo.name} 🎵`;
+      }
+
+      // Rrenderiso
+      if (this.container) {
+        this.container.innerHTML = dashboardClienteTemplate.init();
+      }
+
+      return;
+    }
+
+    // si no esta activo ni logeado 
+    window.location.href = "../index.html";
+    return;
+  }
 };
 
-  dashboardCliente.init();
+// 
+/*
+const botonCerrarSesion = document.getElementById("logout");
+if (botonCerrarSesion) {
+  botonCerrarSesion.addEventListener("click", () => {
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const usuarioActivo = users.find(user => user.isActive === true);
+    
+    if (usuarioActivo) {
+      usuarioActivo.isActive = false;
+      localStorage.setItem("users", JSON.stringify(users));
+    }
+    
+    window.location.href = "../index.html";
+  });
+}
+*/
+
+// ¡Importante! Normalmente se llama así:
+dashboardCliente.init();
